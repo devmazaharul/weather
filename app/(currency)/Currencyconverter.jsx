c358ad24416e19c216614523f381993576bf2da7
jr-programmer <expertmazahrul@gmail.com>
2024-10-16 20:48:30 +0600

    sls

--- app/(currency)/Currencyconverter.jsx
+++ app/(currency)/Currencyconverter.jsx
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from "react";
 import { FaExchangeAlt } from "react-icons/fa";
 
 export default function Currencyconverter() {
+
+
+
   const [currency, setcurrency] = useState({
     from: "USD",
     to: "BDT",
@@ -23,64 +26,67 @@ export default function Currencyconverter() {
       })
       .then((response) => {
         const { data, meta } = response;
-        setcurrent(parseInt(data[currency.to].value));
+        setcurrent(Number(data[currency.to].value).toFixed(2));
       });
   }, [currency]);
 
-
-  function handlechange(e,name){
-
-    setcurrency({...currency,[name]:e})
-    
+  function handlechange(e, name) {
+    setcurrency({ ...currency, [name]: e });
   }
-
-
-
 
   return (
     <div className="bg-gradient-to-r  from-indigo-200  to-purple-300  p-3 border w-full rounded-sm">
-      <h1 className="capitalize font-bold text-center py-3">currency converter</h1>
+      <h1 className="capitalize font-bold text-center py-3">
+        currency converter
+      </h1>
 
-<div className="flex items-center gap-2 w-full mx-auto">
-<select onChange={(e)=>handlechange(e.target.value,"from")} value={currency.from} className="w-[45%] py-2 rounded-sm outline-none">
-    <option value="BDT">BDT</option>
-    <option value="USD">USD</option>
-    <option value="INR">INR</option>
-    <option value="PKR">PKR</option>
-    <option value="eur">EUR</option>
-    <option value="CAD">CAD</option>
-    <option value="AFN">AFN</option>
-    <option value="ALL">ALL</option>
-    <option value="AMD">AMD</option>
-    <option value="ARS">ARS</option>
-    <option value="AUD">AUD</option>
-    <option value="AZN">AZN</option>
-    <option value="BAM">BAM</option>
-    <option value="BGN">BGN</option>
-    <option value="BHD">BHD</option>
-</select>
-<p className="w-[5%] text-green-400"><FaExchangeAlt/></p>
-<select onChange={(e)=>handlechange(e.target.value,"to")} value={currency.to} className="w-[45%] py-2 rounded-sm outline-none" >
-<option value="BDT">BDT</option>
-<option value="USD">USD</option>
-    <option value="INR">INR</option>
-    <option value="PKR">PKR</option>
-    <option value="eur">EUR</option>
-    <option value="CAD">CAD</option>
-    <option value="AFN">AFN</option>
-    <option value="ALL">ALL</option>
-    <option value="AMD">AMD</option>
-    <option value="ARS">ARS</option>
-    <option value="AUD">AUD</option>
-    <option value="AZN">AZN</option>
-    <option value="BAM">BAM</option>
-    <option value="BGN">BGN</option>
-    <option value="BHD">BHD</option>
-</select>
-</div>
-
-
-
+      <div className="flex items-center gap-2 w-full mx-auto">
+        <select
+          onChange={(e) => handlechange(e.target.value, "from")}
+          value={currency.from}
+          className="w-[45%] py-2 rounded-sm outline-none"
+        >
+          <option value="BDT">BDT</option>
+          <option value="USD">USD</option>
+          <option value="INR">INR</option>
+          <option value="PKR">PKR</option>
+          <option value="eur">EUR</option>
+          <option value="CAD">CAD</option>
+          <option value="AFN">AFN</option>
+          <option value="ALL">ALL</option>
+          <option value="AMD">AMD</option>
+          <option value="ARS">ARS</option>
+          <option value="AUD">AUD</option>
+          <option value="AZN">AZN</option>
+          <option value="BAM">BAM</option>
+          <option value="BGN">BGN</option>
+          <option value="BHD">BHD</option>
+        </select>
+        <p className="w-[5%] text-green-400">
+          <FaExchangeAlt />
+        </p>
+        <select
+          onChange={(e) => handlechange(e.target.value, "to")}
+          value={currency.to}
+          className="w-[45%] py-2 rounded-sm outline-none"
+        >
+          <option value="BDT">BDT</option>
+          <option value="USD">USD</option>
+          <option value="INR">INR</option>
+          <option value="PKR">PKR</option>
+          <option value="eur">EUR</option>
+          <option value="CAD">CAD</option>
+          <option value="AFN">AFN</option>
+          <option value="ALL">ALL</option>
+          <option value="AMD">AMD</option>
+          <option value="ARS">ARS</option>
+          <option value="AUD">AUD</option>
+          <option value="AZN">AZN</option>
+          <option value="BAM">BAM</option>
+          <option value="BGN">BGN</option>
+          <option value="BHD">BHD</option>
+        </select>
+      </div>
 
       <input
         onChange={(e) => setamount(e.target.value)}
